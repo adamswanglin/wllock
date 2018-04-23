dda07ce9b3dec90fd6572f2c971fa60779d5281b
王霖_wanglin <wanglin@enmonster.com>
2018-04-23 12:48:22 +0800

    change to redis lua

--- wllock/src/main/java/org/wl/wllock/lock/distribution/RedisLock.java
+++ wllock/src/main/java/org/wl/wllock/lock/distribution/RedisLock.java
@@ -4,10 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
 import org.wl.wllock.config.LockSettings;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wanglin on 2017/3/28.
@@ -16,8 +21,28 @@ import java.util.concurrent.TimeUnit;
 public class RedisLock implements DistributionLock {
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisLock.class);
 
+    /**
+     * setNX + expire的lua脚本.
+     * <p>
+     * setNxWithExpire(key, args[1], args[2]).
+     * key
+     * args[1] 值
+     * args[2] 失效时间，单位：毫秒
+     */
+    private static final String SET_NX_WITH_EXPIRE =
+            "local rst = redis.call('SETNX',KEYS[1],ARGV[1]);"
+                    + "if (rst==1) then redis.call('PEXPIRE', KEYS[1], ARGV[2]); end;"
+                    + " return rst;";
+
+    /**
+     * setNX + expire脚本.
+     */
+    private static RedisScript<Long> SET_NX_WITH_EXPIRE_SCRIPT = new DefaultRedisScript<>(SET_NX_WITH_EXPIRE, Long.class);
+    private static RedisSerializer<Long> LONG_SERIALIZER = new GenericToStringSerializer<>(Long.class);
+
     @Autowired
     private StringRedisTemplate redisTemplate;
+
     @Autowired
     private LockSettings lockSettings;
 
@@ -25,10 +50,15 @@ public class RedisLock implements DistributionLock {
         try {
             long start = System.currentTimeMillis();
             while (true) {
-                if (redisTemplate.opsForValue().setIfAbsent(key, "")) {
-                    redisTemplate.expire(key, lockSettings.getLockTimeMaxMillis(), TimeUnit.MILLISECONDS);
+                List<String> keys = new ArrayList<>(1);
+                keys.add(key);
+                String value = "";
+
+                Long result = redisTemplate.execute(SET_NX_WITH_EXPIRE_SCRIPT, redisTemplate.getStringSerializer(), LONG_SERIALIZER, keys, value, String.valueOf(lockSettings.getLockTimeMaxMillis()));
+                if (result == 1) {
                     return true;
                 }
+
                 long end = System.currentTimeMillis();
                 if (tryMilliSeconds > 0 && tryMilliSeconds < (end - start)) {
                     return false;
